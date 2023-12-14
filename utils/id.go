0e854380ac1194d66448084ef919dba4f0a0d0b6
Dinesh Salunke <dineshsalunke@consultbop.com>
2023-12-14 12:23:45 +0530

    refactor: add read and write methods for id

--- utils/id.go
+++ utils/id.go
@@ -19,3 +19,20 @@ func (id *ID) CompareID(other *ID) bool {
 	return id == other ||
 		(id != nil && other != nil && id.client == other.client && id.clock == other.clock)
 }
+
+func (id *ID) WriteID(encoder *Encoder) {
+	encoder.WriteVarUint(id.client)
+	encoder.WriteVarUint(id.clock)
+}
+
+func ReadID(decoder *Decoder) (*ID, error) {
+	client, err := decoder.ReadVarUint()
+	if err != nil {
+		return nil, err
+	}
+	clock, err := decoder.ReadVarUint()
+	if err != nil {
+		return nil, err
+	}
+	return newID(client, clock), nil
+}
